534974d90c9603225242d9681f3385e3804e0baf
Kishan-Ramani <kishanramani269@gmail.com>
2025-05-27 16:14:20 +0530

    mark subproject as dirty

--- 03login/src/app/api/users/signup/route.ts
+++ 03login/src/app/api/users/signup/route.ts
@@ -16,6 +16,20 @@ export async function POST(requset : NextRequest) {
             return NextResponse.json({error: "User already exists"}, {status: 400});
         }
 
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            email,
+            password : hashedPassword
+        })
+
+        const savedUser = await newUser.save();
+
+        console.log(savedUser)
+
+
     } catch (error : any) {
         return NextResponse.json({error: error.message}, {status : 500})
     }
