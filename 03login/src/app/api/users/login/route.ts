76aa5bb5c112b7664d74474c512df92d23cb92a7
Kishan-Ramani <kishanramani269@gmail.com>
2025-06-02 17:09:32 +0530

    Update package dependencies and improve user login route

--- 03login/src/app/api/users/login/route.ts
+++ 03login/src/app/api/users/login/route.ts
@@ -1,8 +1,8 @@
 import { connectdb } from "@/dbconfig/dbconfig";
-import { NextResponse , NextRequest} from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import User from '@/models/userModel'
 import bcrypt from 'bcryptjs';
-
+import jwt from 'jsonwebtoken';
 
 connectdb();
 
@@ -14,21 +14,37 @@ export async function POST(request: NextRequest) {
 
         const user = await User.find
 
-        if(!user){
+        if (!user) {
             return NextResponse.json({ error: "User not found" }, { status: 404 });
         }
         console.log("User found", user);
-        
+
         const validPassword = await bcrypt.compare(password, user.password);
 
-        if(!validPassword) {
+        if (!validPassword) {
             return NextResponse.json({ error: "Invalid password" }, { status: 401 });
         }
         console.log("Password is valid");
 
-        
+        const tokenData = {
+            id: user._id,
+            email: user.email,
+            username: user.username
+        }
 
-        
+        const token = jwt.sign(tokenData, process.env.TOKNEN_SECRET!, { expiresIn: '1h' });
+
+        const response = NextResponse.json({
+            message: "Login successfuly",
+            success: true
+        })
+
+        response.cookies.set("token", token, {
+            httpOnly: true
+        })
+
+        return response;
+
     } catch (error) {
         return NextResponse.json({ error: "An error occurred" }, { status: 500 });
     }
