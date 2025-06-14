ab12773e8ecbfe66a1fd4d4d02f88f940605554f
Kishan-Ramani <kishanramani269@gmail.com>
2025-06-14 16:37:01 +0530

    Implement code changes to enhance functionality and improve performance

--- 03login/src/app/dashboard/page.jsx
+++ 03login/src/app/dashboard/page.jsx
@@ -2,34 +2,33 @@
 import React from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import toast,{Toaster} from "react-hot-toast";
-
+import toast, { Toaster } from "react-hot-toast";
 
 const page = () => {
+  const router = useRouter();
 
-  const router = useRouter()
-
-  const  logout  = async () => {
+  const logout = async () => {
     try {
-      await axios.get("http://localhost:3000/api/users/logout")
-      router.push("/login")
-      toast.success("logout succesfully")
-    } catch (error) {
+      await axios.get("http://localhost:3000/api/users/logout");
+      toast.success("logout succesfully");
+      router.push("/login");
       
+    } catch (error) {
+      toast.error("Error in logout");
     }
-};
-
+  };
 
   return (
     <>
-     <form className="">
-       <button
-        onClick={logout}
-        className="absolute top-10 right-20 border border-white px-4 py-2 rounded-2xl cursor-pointer"
-      >
-        Logout
-      </button>
-     </form>
+     
+        <button
+          onClick={logout}
+          className="absolute top-10 right-20 border border-white px-4 py-2 rounded-2xl cursor-pointer"
+        >
+          Logout
+        </button>
+      
+      <Toaster />
     </>
   );
 };
--- 05mipl/src/app/gsap/page.jsx
+++ 05mipl/src/app/gsap/page.jsx
@@ -1,21 +1,21 @@
 "use client";
 import React from "react";
 import { gsap } from "gsap";
-import {ScrollTrigger} from "gsap/ScrollTrigger";
-import { useGSAP  } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/SplitText";
 import Header from "../../Components/Header";
 import Navbar from "../../Components/Navbar";
+import { random } from "gsap/gsap-core";
 
-
-gsap.registerPlugin(SplitText,ScrollTrigger);
+gsap.registerPlugin(SplitText, ScrollTrigger);
 const page = () => {
   useGSAP(() => {
     // const tl = gsap.timeline();
 
-    let split = SplitText.create(".pg1text", { type: "words" });
+    let split = SplitText.create(".pg1text", { type: "chars" });
 
-    gsap.from(split.words, {
+    gsap.from(split.chars, {
       y: 100,
       transform: "translateY(100px)",
       color: "green",
@@ -41,26 +41,26 @@ const page = () => {
 
     gsap.from(".page3 .video", {
       transform: "translateY(-120vh)",
-      height:"100vh",
-      width:"95%",
+      height: "100vh",
+      width: "95%",
       duration: 1,
-      marginLeft:0,
-      scrollTrigger:{
-        trigger:".page2",
-        start:"top top",
-        end:"bottom top",
+      marginLeft: 0,
+      scrollTrigger: {
+        trigger: ".page2",
+        start: "top top",
+        end: "bottom top",
         scrub: 2,
         // markers: true,
         toggleAttribute: "restart pause reverse pause",
-      }
-    })
-
+      },
+    });
 
     let page3 = SplitText.create(".pg3text", { type: "chars" });
 
     gsap.from(page3.chars, {
-      x: 50,
+      x: 100,
       duration: 1,
+      color: "green",
       autoAlpha: 0,
       stagger: {
         amount: 0.5,
@@ -71,8 +71,21 @@ const page = () => {
         trigger: ".page3",
         start: "top 50%",
         toggleAttribute: "restart ",
-      }
+      },
     });
+
+    let page4 = SplitText.create(".pg4text", { type: "chars" });
+
+    gsap.from(page4.chars, {
+      x:random(-100,100),
+      y:random(-100,100),
+      duration:1,
+      stagger:{
+        from:random,
+        ease: "power2.in",
+        amount:1
+      }
+    })
   });
 
   return (
@@ -114,9 +127,7 @@ const page = () => {
           </div>
         </div>
 
-        <div className="page2 h-screen w-full flex items-center justify-center relative px-6 py-7">
-          
-        </div>
+        <div className="page2 h-screen w-full flex items-center justify-center relative px-6 py-7"></div>
 
         <div className="page3 h-screen w-full flex items-start justify-center flex-col relative px-6 py-7">
           <div className="video absolute w-[30vw] h-[5vw] ml-[5vw] flex items-center justify-center top-10">
@@ -128,42 +139,51 @@ const page = () => {
               muted
             ></video>
           </div>
-         <div className=" mt-[10vw]">
-           <h1 className="absolute top-0 left-[40%] overflow-hidden">
-            <span className="pg3text">Improve your animation skills</span>
-          </h1>
-          <h1 className="absolute top-[20%] left-[40%] overflow-hidden text-6xl">
-            <span className="pg3text">Motion on websites is a must-</span>
-          </h1>
-          <h1 className="text-6xl overflow-hidden">
-            <span className="pg3text">have these days. Developers are constantly</span>
-          </h1>
-          <h1 className="text-6xl overflow-hidden">
-            <span className="pg3text">crafting new animations that push the</span>
-          </h1>
-          <h1 className="text-6xl overflow-hidden">
-            <span className="pg3text">boundaries of creativity.If you're ready to explore</span>
-          </h1>
-          <h1 className="text-6xl overflow-hidden">
-            <span className="pg3text">the power of GSAP, we’ve got you covered with</span>
-          </h1>
-          <h1 className="text-6xl overflow-hidden">
-            <span className="pg3text">50 unique effects designed to help you master it</span>
-          </h1>
-          <h1 className="text-6xl overflow-hidden">
-            <span className="pg3text">like a pro.</span>
-          </h1>
-         </div>
+          <div className=" mt-[10vw]">
+            <h1 className="absolute top-0 left-[40%] overflow-hidden">
+              <span className="pg3text">Improve your animation skills</span>
+            </h1>
+            <h1 className="absolute top-[20%] left-[40%] overflow-hidden text-6xl">
+              <span className="pg3text">Motion on websites is a must-</span>
+            </h1>
+            <h1 className="text-6xl overflow-hidden">
+              <span className="pg3text">
+                have these days. Developers are constantly
+              </span>
+            </h1>
+            <h1 className="text-6xl overflow-hidden">
+              <span className="pg3text">
+                crafting new animations that push the
+              </span>
+            </h1>
+            <h1 className="text-6xl overflow-hidden">
+              <span className="pg3text">
+                boundaries of creativity.If you're ready to explore
+              </span>
+            </h1>
+            <h1 className="text-6xl overflow-hidden">
+              <span className="pg3text">
+                the power of GSAP, we’ve got you covered with
+              </span>
+            </h1>
+            <h1 className="text-6xl overflow-hidden">
+              <span className="pg3text">
+                50 unique effects designed to help you master it
+              </span>
+            </h1>
+            <h1 className="text-6xl overflow-hidden">
+              <span className="pg3text">like a pro.</span>
+            </h1>
+          </div>
         </div>
 
         <div className="page4 h-screen w-full relative px-6 py-7">
           <div className="w-full flex items-start justify-center">
-              <h1 className="overflow-hidden relative">
-                <i className="ri-corner-down-right-line text-7xl font-light"></i>
-                <span className="text-7xl font-light">Explore Collection</span>
-                
-              </h1>
-              <div className="absolute w-full h-[0.2vw] bg-black top-[8vw] scale-48"></div>
+            <h1 className="overflow-hidden relative">
+              <i className="ri-corner-down-right-line text-7xl font-light"></i>
+              <span className="pg4text text-7xl font-light">Explore Collection</span>
+            </h1>
+            <div className="absolute w-full h-[0.2vw] bg-black top-[8vw] scale-48"></div>
           </div>
         </div>
       </main>
