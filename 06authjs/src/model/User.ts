096ef75663a2a86425c8c7df3d2e07080ae37afe
Kishan-Ramani <kishanramani269@gmail.com>
2025-06-19 17:36:43 +0530

    feat: define User model with schema and interface for user management
    - Created User interface extending Document to define user properties.
    - Implemented UserSchema with validation for username, email, password, verification code, and message handling.
    - Ensured unique constraints and proper data types for user attributes.

--- 06authjs/src/model/User.ts
+++ 06authjs/src/model/User.ts
@@ -15,4 +15,58 @@ const MessageSchema: Schema<Message> = new Schema({
         default:Date.now,
     }
 })
-   
+
+export interface User extends Document{
+    username:string;
+    email:string;
+    password:string;
+    verifieCode:string;
+    verifeCodeExpires:Date;
+    isVerified:boolean;
+    isAcceptedMessages:boolean;
+    messages:Message[];
+}
+
+const UserSchema:Schema<User> = new Schema({
+    username:{
+        type:String,
+        require:[true,"Username required"],
+        trim:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        require:[true,"Email required"],
+        trim:true,
+        unique:true,
+        match:[/^\S+@\S+\.\S+$/,"Invalid email address"],
+    },
+    password:{
+        type:String,
+        require:[true,"Password required"],
+    },
+    verifieCode:{
+        type:String,
+        default:"",
+        required:[true,"Verification code required"],
+    },
+    verifeCodeExpires:{
+        type:Date,
+        required:[true,"Verification code expiration date required"],
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+    },
+    isAcceptedMessages:{
+        type:Boolean,
+        default:true,
+    },
+    messages:{
+        type:[MessageSchema],
+    }
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User",UserSchema);
+
+export default UserModel;
