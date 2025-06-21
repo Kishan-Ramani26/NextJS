785269bf6e435c99f4e520ec893b77a40e8db8cb
Kishan-Ramani <kishanramani269@gmail.com>
2025-06-21 17:26:01 +0530

    fix: add missing newline at end of verifySchema file

---
+++ 06authjs/src/lib/dbConnect.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+const connection: ConnectionObject = {};
+
+const dbconnect = async (): Promise<void> => {
+    if (connection.isConnected) return;
+
+    try {
+         const db = await mongoose.connect(process.env.MONGODB_URI || "");
+         connection.isConnected = db.connections[0].readyState;
+
+         console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
