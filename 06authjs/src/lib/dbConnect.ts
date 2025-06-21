7bdc81bbefc6647b31421ad69a3447806dd1e91e
Kishan-Ramani <kishanramani269@gmail.com>
2025-06-21 17:40:53 +0530

    fix: update dbConnect to include options in mongoose connection and export the function
    - Added an empty options object to the mongoose.connect call for future configuration.
    - Exported the dbconnect function for use in other modules.

--- 06authjs/src/lib/dbConnect.ts
+++ 06authjs/src/lib/dbConnect.ts
@@ -9,12 +9,14 @@ const dbconnect = async (): Promise<void> => {
     if (connection.isConnected) return;
 
     try {
-         const db = await mongoose.connect(process.env.MONGODB_URI || "");
-         connection.isConnected = db.connections[0].readyState;
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+        connection.isConnected = db.connections[0].readyState;
 
-         console.log("Connected to MongoDB");
+        console.log("Connected to MongoDB");
     } catch (error) {
         console.log(error);
         process.exit(1);
     }
 }
+
+export default dbconnect;
