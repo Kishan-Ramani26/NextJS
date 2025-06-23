bf053555eb044dffbadad84fc700dd29bfcc1206
Kishan-Ramani <kishanramani269@gmail.com>
2025-06-23 17:09:23 +0530

    feat: integrate verification email functionality and update dependencies
    - Added the sendVerificationEmail function to handle sending verification emails using the resend service.
    - Included the VerificationEmail component for rendering the email content.
    - Updated package.json and package-lock.json to include @react-email/components and related dependencies.
    - Modified tsconfig.json to include the new verification email component in the TypeScript compilation.

--- 06authjs/src/helpers/sendVerficationEmail.ts
+++ 06authjs/src/helpers/sendVerficationEmail.ts
@@ -1,5 +1,31 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/verficationEmail";
+import { ApiResponse } from "@/types/apiResponse";
 
-const sendVerificationEmail = async()=>{
-    
+
+export async function sendVerificationEmail
+    (
+        email: string,
+        username: string,
+        verfycode: string
+    ): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: email,
+            subject: "Verify your email",
+            react: VerificationEmail({ username, otp: verfycode })
+        })
+        return {
+            success: true,
+            message: "Verification email sent successfully"
+        }
+    } catch (error) {
+        console.log("Error sending verification email: " + error);
+        return {
+            success: false,
+            message: "Error sending verification email"
+        }
+    }
+
 }
