21f7d5f6575db96ce1a319cbefca42846a84f803
Kishan-Ramani <kishanramani269@gmail.com>
2025-07-01 17:22:53 +0530

    feat: enhance authentication options with improved user verification and error handling
    - Added password field to credentials provider for login.
    - Implemented user verification checks during the authorization process, ensuring users are verified before login.
    - Enhanced error handling for login failures, providing clearer feedback for incorrect credentials and unverified users.
    - Updated import paths for consistency and accuracy.

--- 06authjs/src/app/api/auth/[...nextauth]/options.ts
+++ 06authjs/src/app/api/auth/[...nextauth]/options.ts
@@ -1,18 +1,47 @@
-import NextAuth,{ NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bycrypt from "bcryptjs";
 import dbconnect from "@/lib/dbConnect";
-import User from "@/models/User";
-import { email } from "zod/v4";
+import User from "@/model/User";
+import { email, promise } from "zod/v4";
 
 export const authOptions: NextAuthOptions = {
-    providers:[
+    providers: [
         CredentialsProvider({
-            id:"credentials",
-            name:"Credentials",
-            credentials:{
-                email:{
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "text"
+                },
+                password: {
+                    label: "Password",
+                    type: "password"
+                }
+            },
+            async authorize(credentials: any): Promise<any> {
+                await dbconnect();
+                try {
+                    const users = await User.findOne({
+                        $or: [{ email: credentials.identifier }, { username: credentials.identifier }]
+                    })
+                    if (!users) {
+                        throw new Error("User not found");
+                    }
+                    if (!users.isVerified) {
+                        throw new Error("User not verified");
+                    }
+                    const isPasswordCorrect = await bycrypt.compare(credentials.password, users.password)
                     
+                    if(isPasswordCorrect){
+                        return users;
+                    }else{
+                        throw new Error("Incorrect password");
+                    }
+
+                } catch (error) {
+                    throw new Error("Failed to login" + error);
                 }
             }
         })
