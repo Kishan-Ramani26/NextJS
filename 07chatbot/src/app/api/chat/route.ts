1c701813d35cd47e8c271388bfa18f42e5018774
Kishan-Ramani26 <kishanramani269@gmail.com>
2025-08-21 19:32:35 +0530

    feat: add TypeScript support and improve chat API integration

--- 07chatbot/src/app/api/chat/route.ts
+++ 07chatbot/src/app/api/chat/route.ts
@@ -1,18 +1,24 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_AI_KEY
+    apiKey: process.env.OPENAI_API_KEY
 })
 
-export async function POST(reqest) {
+export async function POST(request: Request) {
     try {
-        const { message } = await reqest.json();
+        const { message } = await request.json();
 
         const completions = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: [{ role: 'user', content: message }]
         })
-    } catch (error) {
 
+        return Response.json({
+            response: completions.choices[0].message.content
+        })
+    } catch (error) {
+        return Response.json({
+            error: "Failed to get request"
+        }, { status: 500 })
     }
 }
--- 07chatbot/src/app/page.js
+++ 07chatbot/src/app/page.js
@@ -1,9 +1,67 @@
-import Image from "next/image";
+'use client'
+import axios from "axios";    
+import { useState } from "react";
 
 export default function Home() {
+
+  const [message, setmessage] = useState("")
+  const [response, setResponse] = useState("")
+  const [loading, setloading] = useState(false) // Start as false
+
+  const handleChat = async () => {
+    try {
+      setloading(true)
+      setResponse("") // Clear previous response
+
+      console.log("Sending message:", message)
+
+      // const res = await axios.post(
+      //   "/api/chat", 
+      //   { message }, 
+      //   { headers: { "Content-Type": "application/json" } },
+      //   {body: JSON.stringify({ message })}
+      // );
+
+      const res = await fetch("/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message })
+      });
+
+      const data = await res.json();
+
+      setResponse(data.response);
+
+    } catch (error) {
+      console.log("fail to get response")
+      setResponse("fail to get response")
+    }  
+  }
+
   return (
-   <div>
-      <h1 className="text-center">Chat Bot</h1>
-   </div>
+    <div>
+      <h1>Home Page</h1>
+      <div>
+        <input 
+          type="text" 
+          placeholder="Type your message here..." 
+          value={message} 
+          onChange={(e) => setmessage(e.target.value)} 
+          className="border-white border-2 p-2"
+        />
+        <button 
+          onClick={handleChat} 
+          className="border-white border-2 p-2 cursor-pointer"
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Send"}
+        </button>
+      </div>
+      <div>
+        {response && <p>Response: {response}</p>}
+      </div>
+    </div>
   );
 }
